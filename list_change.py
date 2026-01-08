414a508a5266a8be4ea54c3344bfea81c49f776e
OleksandrByhkov <bychkovshura924@gmail.com>
2026-01-08 12:33:08 +0200

    Add files via upload

---
+++ list_change.py
@@ -0,0 +1,9 @@
+lst = [1, 22, 13, 45, 58]
+lst2 = []
+lst3 = [1, 2, 3, 4]
+
+
+print([lst[:(len(lst) + 1) // 2], lst[(len(lst) + 1) // 2:]])
+print([lst2[:(len(lst2) + 1) // 2], lst2[(len(lst2) + 1) // 2:]])
+print([lst3[:(len(lst3) + 1) // 2], lst3[(len(lst3) + 1) // 2:]])
+
